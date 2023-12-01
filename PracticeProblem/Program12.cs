86ac986ab7f012db60aac2c5cc6c6d87ef3e2e34
sachin2k01 <thenamesachin@gmail.com>
2023-12-02 00:15:01 +0530

    program 12 is completed without using loop printing a number 1 to 100

---
+++ PracticeProblem/Program12.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program12
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing Number from 1 to 100");
+            int i = 1;
+            NumPrint(i);
+        }
+
+        public static void NumPrint(int n)
+        {
+            if(n <= 100)
+            {
+                Console.Write(n+ " ");
+                 NumPrint(n+1);
+            }
+
+        }
+    }
+}
