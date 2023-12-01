0dd977c64386b6aad7d58734d0f1a4a9e08ed0bf
sachin2k01 <thenamesachin@gmail.com>
2023-12-01 23:47:31 +0530

    Finish with program 3 taking user input print all the thing in UpperCapse and SmallCase

---
+++ PracticeProblem/Program3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How Many place you want to visit");
+            int size=int.Parse(Console.ReadLine());
+            string[] place=new string[size];
+            Console.WriteLine("Enter Place You Want Visit");
+            for(int i=0; i<size; i++)
+            {
+                place[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Your Fav Place in Capital Letter");
+            foreach(string cap in  place)
+            {
+                Console.WriteLine($"{cap.ToUpper()}");
+            }
+            Console.WriteLine("Your Fav Place in small Letter");
+            foreach (string small in place)
+            {
+                Console.WriteLine($"{small.ToLower()}");
+            }
+        }
+    }
+}
