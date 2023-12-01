8560e0627f722c501b8545179e496253132692ca
sachin2k01 <thenamesachin@gmail.com>
2023-12-01 23:36:51 +0530

    Completed the program 4 printing entered number as a string 10 times

---
+++ PracticeProblem/Program4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number ");
+            string num=Console.ReadLine();
+            Console.WriteLine("Printing Entered Number 10 Times");
+            for(int i = 0;i<10;i++)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
