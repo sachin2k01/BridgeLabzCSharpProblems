95531bddeaa41706bd1df21f55cec24b41a9d57f
sachin2k01 <thenamesachin@gmail.com>
2023-12-01 23:29:22 +0530

    first program is Completed that is printing friends name

---
+++ PracticeProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number How many friends names to insert ");
+            int size=int.Parse(Console.ReadLine());
+            string[] names=new string[size];
+            for(int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Your Friends Names Are");
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
