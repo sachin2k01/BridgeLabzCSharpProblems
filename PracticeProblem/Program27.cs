66bfb8a7c2f6473ee3011bf1903f0923712f6b2b
sachin2k01 <thenamesachin@gmail.com>
2023-12-03 14:49:25 +0530

    Completed program 27 and 33

---
+++ PracticeProblem/Program27.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program27
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number of Days to Convert");
+            int days=int.Parse(Console.ReadLine());
+
+            int year = days / 365;
+            Console.WriteLine($"Years: {year}");
+
+        
+
+            int month = days / 30;
+            Console.WriteLine($"Months : {month}");
+
+            int weeks = days/7;
+            Console.WriteLine($"Weeks : {weeks}");
+        }
+    }
+}
---
+++ PracticeProblem/Program33.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program33
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array size");
+            int size=int.Parse(Console.ReadLine());
+            int[] ar=new int[size];
+            Console.WriteLine("Enter Array elements to Count sum of its number");
+            for(int i=0; i<size; i++)
+            {
+                ar[i]=int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            foreach(int i in ar)
+            {
+                sum+=i;
+            }
+            Console.WriteLine($"Sum of the Array elements is :{sum}");
+        }
+    }
+}
