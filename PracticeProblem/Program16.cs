aa99497222d873a9030f3cc287205dc7593f4061
sachin2k01 <thenamesachin@gmail.com>
2023-12-03 12:15:43 +0530

    Completed the program 16 that is find an average of float array

---
+++ PracticeProblem/Program16.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program16
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the float array size");
+            int size=int.Parse(Console.ReadLine());
+            float[] arr=new float[size];
+            Console.WriteLine("Enter the Float value for an Array");
+            for(int i = 0;i<size;i++)
+            {
+                arr[i]=float.Parse(Console.ReadLine());
+            }
+            Program16 p=new Program16();
+
+            float res=p.Avg(arr, size);
+            Console.WriteLine($"Average of array is :{res}");
+
+        }
+
+        public float Avg(float[] a, int size)
+        {
+            float avg = 0.0f;
+            for(int i=0;i<size;i++)
+            {
+                avg += a[i];
+            }
+
+            float totalavg = avg / size;
+            return totalavg;
+        }
+    }
+}
