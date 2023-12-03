0b8748358f818146ab086f6f53d1a590e900f3dc
sachin2k01 <thenamesachin@gmail.com>
2023-12-03 11:21:53 +0530

    completed second problem to find sqaure of sum and diffrence of two number

--- PracticeProblem/Program2.cs
+++ PracticeProblem/Program2.cs
@@ -10,7 +10,19 @@ namespace PracticeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Second Program");
+            Console.WriteLine("Enter First Number");
+            double num1=double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double sum=num1+num2;
+            double diffrence=num1-num2;
+
+            double sumofsquare=Math.Pow(sum,2);
+            double diffsquare=Math.Pow(diffrence,2);
+
+            Console.WriteLine($"Square of sum of the two number is {sumofsquare}");
+            Console.WriteLine($"square of diffrence of two number is {diffsquare}");
         }
 
     }
