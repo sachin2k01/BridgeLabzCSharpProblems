6661d88c141df0248a2fbb945fc9f002e30a226c
sachin2k01 <thenamesachin@gmail.com>
2023-12-03 15:12:34 +0530

    program 29 is completed and program 28 is started

---
+++ PracticeProblem/Program28.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program28
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number to Find a square root of the Number");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num <2) 
+            {
+                Console.WriteLine("Square root of enterd number is :" + num);
+            }
+
+            int sroot = ((num) * 1 / 2);
+            Console.WriteLine(sroot);
+        }
+    }
+}
---
+++ PracticeProblem/Program29.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program29
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Numbers Between 1 to 100");
+            Program29 pp=new Program29();
+            for(int i = 1; i <=100;i++)
+            {
+                bool res=pp.prime(i);
+                if(res==true)
+                {
+                    Console.WriteLine($"{i} is a Prime Number");
+                }
+            }
+        }
+
+        public bool prime(int n)
+        {
+            if(n == 0 || n == 1)
+            {
+                return false;
+            }
+            else if(n == 2)
+            {
+                return true;
+            }
+            else
+            {
+                for(int i=2;i<n;i++)
+                {
+                    if(n%i==0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
