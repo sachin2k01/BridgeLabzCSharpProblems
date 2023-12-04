5f69b12f4d9f068a54aaee7600dd4af08175f2bd
sachin2k01 <thenamesachin@gmail.com>
2023-12-04 23:00:18 +0530

    Program 41 is completd find an second and minutes from hour

---
+++ PracticeProblem/Program41.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    class Program41
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an Hour to find minute and seconds");
+            int hour=int.Parse(Console.ReadLine());
+
+            int minute = hour * 60;
+            Console.WriteLine("Minutes: " + minute);
+            int second=hour * 3600;
+            Console.WriteLine("Seconds :" + second);
+        }
+    }
+}
