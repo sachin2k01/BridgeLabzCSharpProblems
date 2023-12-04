6cf16106620838fd4374a0841c4e158e7a2dc7ff
sachin2k01 <thenamesachin@gmail.com>
2023-12-04 23:11:07 +0530

    program 7 is complete perimeter and area of an circle

---
+++ PracticeProblem/Program7.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    class Program7
+    {
+        static void Main(string[] args)
+        {
+            Circle c=new Circle();
+            Console.WriteLine("Enter a radius of the circle");
+            int r=int.Parse(Console.ReadLine());
+            c.Perimeter(r);
+            c.Area(r);
+        }
+    }
+
+    internal class Circle
+    {
+        public void Perimeter(double r)
+        {
+            double peri = 2 * r * Math.PI;
+            Console.WriteLine("Perimeter of the Circle is: " + peri);
+        }
+
+        public void Area(double r)
+        {
+            double area = Math.PI * r*r;
+            Console.WriteLine("Area of the Circle is: " + area);
+        }
+    }
+}
