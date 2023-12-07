4c6df44b2d9040f498fa3e457d8720dccea960c9
sachin2k01 <thenamesachin@gmail.com>
2023-12-07 23:56:19 +0530

    program 31 is completed converting celcius into f and kelvin

---
+++ PracticeProblem/Program31.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program31
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a temperature in celcius");
+            float c=float.Parse(Console.ReadLine());
+
+            float f = c*(9 / 5) * 32;
+            float k = c + 273.15f;
+
+            Console.WriteLine($"Temperature in Fahreneit is: {f}");
+            Console.WriteLine($"Temperature in kelvin is: {k}");
+        }
+    }
+}
