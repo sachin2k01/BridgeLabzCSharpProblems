911d46fb370c7b1782b98fc68254f9a4a3c5dcd6
sachin2k01 <thenamesachin@gmail.com>
2023-12-15 23:21:36 +0530

    Banking Transaction App is Completed

---
+++ PS2/BankApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS2
+{
+    public  class BankApp
+    {
+        static void Main(string[] args)
+        {
+            
+            //creating Saving Account
+            SavingsAccount savingsAccount = new SavingsAccount("SAV-123456");
+            Console.WriteLine("Creating Savings Account: "+savingsAccount.AccountNumber);
+            Console.WriteLine("Depositing Amount");
+            savingsAccount.Deposit(1000);
+            Console.WriteLine("Withdraw Amount");
+            savingsAccount.WithDraw(200);
+           
+
+            
+
+            //Creating checking accounts
+            
+            CheckingAccount checkingAccount = new CheckingAccount("CHK-789012");
+            Console.WriteLine("Creating Checking Account: "+checkingAccount.AccountNumber);
+            Console.WriteLine("Depositing Into Checking Account");
+            checkingAccount.Deposit(500);
+            Console.WriteLine("Withdreaw Amount");
+            checkingAccount.WithDraw(100);
+
+
+
+            Console.WriteLine("Transferring Amount between Acounts");
+            // Transfer between accounts
+            savingsAccount.Transfer(checkingAccount, 300);
+            checkingAccount.Transfer(savingsAccount, 150);
+
+
+        }
+    }
+}
---
+++ PS2/BankOperation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS2
+{
+    abstract class Account
+    {
+        private string accNo;
+
+        public string AccountNumber
+        {
+            get {  return accNo; }
+           private set { accNo = value; }
+        }
+
+        public decimal Balance { get; protected set; }
+
+        public Account(string accountno)
+        {
+            AccountNumber = accountno;
+            Balance = 0;
+           
+        }
+
+        public virtual void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposited: {amount} Current Balance: {Balance}");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Amount");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public virtual void WithDraw(decimal amount)
+        {
+            if (amount > 0 && Balance>amount) 
+            {
+                Balance -= amount;
+                Console.WriteLine($"Amount Withdrawl is: {amount} Balance is: {Balance}");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+
+        public abstract void Transfer(Account destinationaccount, decimal amount);
+
+    }
+
+    class SavingsAccount : Account
+    {
+        public SavingsAccount(string acountNumber) : base(acountNumber) { }
+
+        public override void Transfer(Account destinationaccount, decimal amount)
+        {
+            if(amount>0 && Balance>=amount)
+            {
+                Balance -= amount;
+                destinationaccount.Deposit(amount);
+                Console.WriteLine($"Transferred: {amount} to account: {destinationaccount.AccountNumber}");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid transfer amount or Insufficient Balance");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class CheckingAccount : Account
+    {
+        public CheckingAccount(string accountNumber) : base(accountNumber) { }
+
+        public override void Transfer(Account destinationaccount, decimal amount)
+        {
+            if(amount> 0 && Balance>=amount ) 
+            {
+                Balance -= amount;
+                destinationaccount.Deposit(amount);
+                Console.WriteLine($"Transferred: {amount} to Account :{destinationaccount.AccountNumber} Current Balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Transfer Amount or Insufficient Balance");
+            }
+            Console.WriteLine();
+        }
+    }
+}
