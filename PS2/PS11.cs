a79fefffcaeff7c2b1655443da33a7764fc394fe
sachin2k01 <thenamesachin@gmail.com>
2023-12-15 22:41:45 +0530

    Program 11 all the operation is done and Data is Added and Fetch successfully

--- PS2/PS11.cs
+++ PS2/PS11.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace PS2
 {
@@ -10,6 +7,41 @@ namespace PS2
     {
         static void Main(string[] args)
         {
+
+            // Create students
+            Student student1 = new Student(4, "Amith", 20, "alice@example.com");
+            Student student2 = new Student(2, "Sumith", 21, "bob@example.com");
+
+            //Create Teacher
+            Teacher teacher = new Teacher("Mr. Samarth", 35, "samarth@example.com", "Math");
+
+            // Create courses
+            Course mathCourse = new Course("Mathematics");
+            Course physicsCourse = new Course("Physics");
+
+
+            // Enroll students in courses
+            student1.EnrolledCourses(mathCourse);
+            student2.EnrolledCourses(physicsCourse);
+
+            // Teacher grades students
+            teacher.GradeStudent(student1, mathCourse, 90);
+            teacher.GradeStudent(student2, physicsCourse, 95);
+
+            // Students check their grades
+            Console.WriteLine("Grades for student 1:");
+            student1.GetGrades();
+            Console.WriteLine();
+
+            Console.WriteLine("\nGrades for student 2:");
+            student2.GetGrades();
+            Console.WriteLine();
+
+            // Teacher sends a message to a student
+
+            
+            teacher.SetMessageToStudent(student2, "Keep up the good work!");
+
 
         }
     }
