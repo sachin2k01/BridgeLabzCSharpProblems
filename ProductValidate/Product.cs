64453a7121f53578f9d2cab188620263b6b86b8a
sachin2k01 <thenamesachin@gmail.com>
2023-12-18 21:48:44 +0530

    product validation

---
+++ ProductValidate/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace ProductValidate
+{
+    public class Product
+    {
+        [Required(ErrorMessage ="Product Id is Required")]
+        public int ProductId {  get; set; }
+
+        [Required(ErrorMessage ="Product Name is Required")]
+        [StringLength(50,ErrorMessage ="Product Name should be Less than 50 Character")]
+        public string ProductName { get; set; }
+
+        [Range(0,double.MinValue,ErrorMessage ="price should be a Positive Number")]
+        public double price {  get; set; }  
+
+        public DateTime CreatedAt { get; set; }= DateTime.Now;
+
+    }
+}
---
+++ ProductValidate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductValidate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.price = 1459;
+            product.ProductName = "Watch";
+            product.ProductId = 54;
+            product.CreatedAt = DateTime.Now;
+
+            var productvalidate=new ValidationContext(product);
+            var validateresult=new System.Collections.Generic.List<ValidationResult>();
+
+            bool iSvalid=Validator.TryValidateObject(product,productvalidate,validateresult);
+
+            if (iSvalid)
+            {
+                System.Console.WriteLine("Product are valid");
+            }
+            else
+            {
+                {
+                    System.Console.WriteLine("Not a Valid Product");
+                    foreach(var item in validateresult)
+                    {
+                        System.Console.WriteLine(item.ErrorMessage);
+                    }
+                }
+            }
+        }
+    }
+}
