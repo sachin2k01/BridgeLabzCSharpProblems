e34cde5140d7d344fac9037d91b08e992a01c7aa
sachin2k01 <thenamesachin@gmail.com>
2023-12-18 21:28:47 +0530

    validate object using Data annotation

---
+++ DataAnnotation/DataAnnotationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotation
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="Name is Required")]
+        [StringLength(50, ErrorMessage ="Name Should be Less than 50 characters")]
+        [Display(Name ="Full Name")]
+        public string Name { get; set; }
+
+        [Range(18,65, ErrorMessage ="Age should be between 18 and 65")]
+        public int Age {  get; set; }
+
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$",ErrorMessage = "Invalid email format.")]
+        public string Email {  get; set; }
+
+    }
+}
---
+++ DataAnnotation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace DataAnnotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 25;
+            person.Name = "Sachin";
+            person.Email = "Sachin@gmail.com";
+
+            var validate=new ValidationContext(person);
+            var validateresult=new System.Collections.Generic.List<ValidationResult>();
+
+            bool Isvalid=Validator.TryValidateObject(person, validate,validateresult,true);
+            if (Isvalid == true)
+            {
+                Console.WriteLine("Validation Passed");
+            }
+            else
+            {
+                Console.WriteLine("Validation Failed");
+                foreach (ValidationResult result in validateresult)
+                {
+                    Console.WriteLine(result.ErrorMessage);
+                }
+            }
+
+
+        }
+    }
+}
