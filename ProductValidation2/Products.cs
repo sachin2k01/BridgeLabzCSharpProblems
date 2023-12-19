85b3044b232bad63702c5446d7f5c8cb2d19428e
sachin2k01 <thenamesachin@gmail.com>
2023-12-19 19:06:12 +0530

    Product validation with other Type of validation

---
+++ ProductValidation2/Products.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductValidation2
+{
+    public class Products
+    {
+        [Required(ErrorMessage = "Product ID is required.")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required.")]
+        public string ProductName { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Price should be a positive number.")]
+        public decimal Price { get; set; }
+
+        [DiscountLessThanPrice(ErrorMessage="Discount Should be Less Than Price")]
+        public decimal Discount { get; set; }
+    }
+    public class DiscountLessThanPrice : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var product = (Products)validationContext.ObjectInstance;
+            decimal discount = (decimal)value;
+
+            if (discount > product.Price)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ ProductValidation2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductValidation2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscountLessThanPrice discountLessThanPrice = new DiscountLessThanPrice();
+            Products products = new Products();
+        }
+    }
+
+    
+
+}
